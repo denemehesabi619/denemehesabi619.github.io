5e8609a697a05882790991f6bc31f7560bd90029
siarkonyar <siarkonyar@gmail.com>
2025-02-04 01:54:44 +0000

    video added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,9 +8,9 @@ export default function Body() {
 		<div className='cozy-lofi d-flex flex-column flex-lg-row mb-4'>
 			<img src={cozyLofi} className='cozy-lofi-gif'/>
 			<div className='w-100 pl-lg-5 pt-lg-0 pt-3'>
-				<iframe src="https://www.youtube.com/embed/Q7pf_uHvlj4?si=I7oa1ajEP2S_XslU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/0WeC0myrxGE?si=Qw4WErYVylgdy-BZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 			</div>
-			
+
 		</div>
 	</div>
   )
